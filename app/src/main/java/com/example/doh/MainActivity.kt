e9db7e1be24baa00ace3f2dd549b3ffa65a7ac0f
iankang <ianngech@gmail.com>
2021-08-03 23:43:30 +0300

    added dependencies

--- app/src/main/java/com/example/doh/MainActivity.kt
+++ app/src/main/java/com/example/doh/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.doh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController
     }
+
 }
